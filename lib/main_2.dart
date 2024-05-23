b3964eaacb043ff764a7779020130aabb043c8ea
ragib <ragibn5@gmail.com>
2024-05-23 16:09:43 +0600

    Added another low-level component implementation (for 2nd statement).

--- lib/main_2.dart
+++ lib/main_2.dart
@@ -6,14 +6,14 @@ import 'package:flutter/material.dart';
 /// <b>Abstractions should not depend on details.
 /// Details should depend on abstractions<br></b>
 /// <p>
-/// Here, we have introduced abstraction to the low level components
-/// and made the high level abstraction depend on this low level abstraction
-/// rather than specific implementation details of the individual concrete
-/// implementations (of low level components).
-/// <br><br>
-/// This made our higher level component fully independent on any specific
-/// implementation of a low level component, making it testable in isolation,
-/// and increasing the flexibility.
+/// We've now added another implementation of [UserPreference] and used it.
+/// So, now(from 3rd step), the [UserPreferenceManager] can work with any
+/// [UserPreference] implementation. Also, notice that we did not have to change
+/// anything on any abstractions, or other implementations.
+/// We just added a new implementation of existing abstraction and used it.
+/// So, we can now test our [UserPreferenceManager] in isolation,
+/// with any implementation of [UserPreference], with mock
+/// implementations as well.
 void main() {
   // memory
   final memoryUserPreference = MemoryUserPreference();
@@ -36,6 +36,16 @@ void main() {
 
   final newFileUser = User(uid: "uid-1234");
   fileUserPreferenceService.setUser(newFileUser);
+
+  // db
+  final dbUserPreference = DbUserPreference();
+  final dbUserPreferenceService = UserPreferenceManagerImpl(dbUserPreference);
+
+  final currentDbUser = dbUserPreferenceService.getUser();
+  debugPrint(currentDbUser.toString());
+
+  final newDbUser = User(uid: "uid-1234");
+  dbUserPreferenceService.setUser(newDbUser);
 }
 
 ////////////////////
@@ -62,6 +72,20 @@ class MemoryUserPreference implements UserPreference {
 }
 
 class FileUserPreference implements UserPreference {
+  @override
+  User getUser() {
+    // ...
+    return User(uid: "uid-101010");
+  }
+
+  @override
+  bool setUser(User user) {
+    // ...
+    return true;
+  }
+}
+
+class DbUserPreference implements UserPreference {
   @override
   User getUser() {
     // ...
