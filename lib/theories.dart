332a7b11a09300ac028ed4e38bd7d9fd7be8bc72
ragib <ragibn5@gmail.com>
2024-05-23 13:11:41 +0600

    Initial commit

---
+++ lib/theories.dart
@@ -0,0 +1,3 @@
+/// Dependency inversion principle statement:
+/// 1. High-level modules should not depend directly on low-level modules. Both should depend on abstractions.
+/// 2. Abstractions should not depend on details. Details should depend on abstractions.
